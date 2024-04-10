58cf5f89fdb71382db5d0ff07600ff0ec912a802
Angel Mendez <110472083+Enyel04@users.noreply.github.com>
2024-04-10 10:40:47 -0400

    Iniciando Carrito de Compras con Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+ 
+  return (
+    <>
+    <h1 className=" btn-danger">Hola Mundo</h1>
+
+    <button className=" btn-danger"> peligro</button>
+    </>
+  )
+}
+
+export default App
